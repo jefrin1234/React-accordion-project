1b5798fd3691de9ea00dcc7cb96db87b820c29ed
jefrin james <jamesjefrin13@gmail.com>
2024-06-12 23:41:56 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Accordion from './Components/accordion';
+
+function App() {
+  return (
+    <div className="App">
+ 
+    <h1>Accordion</h1>
+
+      {/* Accordion component */}
+      <Accordion/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "The capital of France is Paris."
+  },
+  {
+    id: 2,
+    question: "What is the largest planet in our solar system?",
+    answer: "The largest planet in our solar system is Jupiter."
+  },
+  {
+    id: 3,
+    question: "Who wrote 'To Kill a Mockingbird'?",
+    answer: "Harper Lee wrote 'To Kill a Mockingbird'."
+  },
+  {
+    id: 4,
+    question: "What is the boiling point of water?",
+    answer: "The boiling point of water is 100°C (212°F) at standard atmospheric pressure."
+  }
+];
+
+export default data;
